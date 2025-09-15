c4167c0a591cc4ae20fc72b901c558d1bf0c8559
Kauã Sanches <119595660+kauasanches@users.noreply.github.com>
2025-09-15 11:38:57 -0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,30 @@
+<?php
+include "conexao.php"; // inclui o arquivo de conexão
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Formulário</title>
+</head>
+<body>
+    <div class="container">
+
+        <form>
+
+            <h1>Login</h1>
+            <input type="text">
+            <input type="password">
+            <input type="email">
+
+            <button type="submit">
+                Enviar
+            </button>
+        </form>
+
+    </div>
+</body>
+</html>
