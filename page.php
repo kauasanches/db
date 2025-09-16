43e096509561f58a204155865f93d00b7bc1270b
Kauã Sanches <119595660+kauasanches@users.noreply.github.com>
2025-09-16 12:15:33 -0300

    Add files via upload

--- page.php
+++ page.php
@@ -3,27 +3,29 @@ include "conexao.php"; // inclui o arquivo de conexão
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="./styles/style-form.css">
     <title>Formulário</title>
 </head>
 <body>
     <div class="container">
 
-        <form>
-
-            <h1>Login</h1>
-            <input type="text">
-            <input type="password">
-            <input type="email">
-
-            <button type="submit">
-                Enviar
-            </button>
-        </form>
+        <div class="container-form">
+            <form>
+    
+                <h1>Login</h1>
+                <input type="text">
+                <input type="password">
+                <input type="email">
+    
+                <button type="submit" class="btn-submit">
+                    Enviar
+                </button>
+            </form>
+        </div>
 
     </div>
 </body>
